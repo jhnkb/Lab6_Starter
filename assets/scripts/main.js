bd589b55ef86c6008885daeb02165898aa0e9901
jhnkb <147684717+jhnkb@users.noreply.github.com>
2026-05-11 17:54:14 -0700

    fix: retreiving recipe data by using getRecipesFromStorage function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,7 @@
 // main.js
 
+// const { createElement } = require("react");
+
 // Run the init() function when the page has loaded
 window.addEventListener("DOMContentLoaded", init);
 
@@ -25,7 +27,13 @@ function getRecipesFromStorage() {
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
 	let JSON_recipes = localStorage.getItem('recipes');
+
+	if (!JSON_recipes){
+		return [];
+	}
+
 	let parsed_recipes = JSON.parse(JSON_recipes);
+
 	return parsed_recipes;
 }
 
@@ -62,6 +70,10 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+
+	let recipe_str = JSON.stringify(recipes);
+
+	localStorage.setItem('recipes', recipe_str)
 }
 
 /**
@@ -70,21 +82,63 @@ function saveRecipesToStorage(recipes) {
  */
 function initFormHandler() {
 	// B2. TODO - Get a reference to the <form> element
+	let form_element = document.getElementById('new-recipe');
+
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
-	// Steps B4-B9 will occur inside the event listener from step B3
-	// B4. TODO - Create a new FormData object from the <form> element reference above
-	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
-	//            make this easier to read), and then extract the keys and corresponding
-	//            values from the FormData object and insert them into recipeObject
-	// B6. TODO - Create a new <recipe-card> element
-	// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-	// B8. TODO - Append this new <recipe-card> to <main>
-	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-	//            then save the recipes array back to localStorage
+
+	form_element.addEventListener("submit", (event) => 
+		{
+
+
+			// Steps B4-B9 will occur inside the event listener from step B3
+			// B4. TODO - Create a new FormData object from the <form> element reference above
+			let new_form = new FormData(form_element);
+
+			// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
+			//            make this easier to read), and then extract the keys and corresponding
+			//            values from the FormData object and insert them into recipeObject
+
+			let recipeObject = Object.fromEntries(new_form.entries());
+
+			// B6. TODO - Create a new <recipe-card> element
+
+			let newRecipeCard = document.createElement('recipe-card');
+
+			// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+
+			newRecipeCard.data = recipeObject;
+
+			// B8. TODO - Append this new <recipe-card> to <main>
+			
+			let ref_main = document.querySelector('main');
+			ref_main.append(newRecipeCard);
+
+			// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
+			//            then save the recipes array back to localStorage
+
+			let recipes = getRecipesFromStorage();
+
+			recipes.push(recipeObject);
+
+			localStorage.setItem('recipes', JSON.stringify(recipes));
+		});
+
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
+	let clearButton = document.querySelector('button[type="button"]');
+
 	// B11. TODO - Add a click event listener to clear local storage button
-	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	// B13. TODO - Delete the contents of <main>
+
+	clearButton.addEventListener("click", () => {
+		// Steps B12 & B13 will occur inside the event listener from step B11
+		// B12. TODO - Clear the local storage
+		localStorage.clear();
+
+		// B13. TODO - Delete the contents of <main>
+		let ref_main = document.querySelector('main');
+
+		ref_main.innerHTML = '';
+	});
+	
+	
 }
